ebf5cd0095c5daea81f1527e0bd000a17724e09d
TheMagicalPlace <chris621@msu.edu>
2019-07-29 22:20:03 -0400

    cleanup of move_selector , still needs to be tested

--- Chess1.5.py
+++ Chess1.5.py
@@ -28,27 +28,9 @@ class Chessgame():
 
     def setup(self):
         if self.isAI == False:
-            self._board_setup()  # sets up the starting board
-            self._position_setup()  # initiallizes the chess piece objects
+            self._setup()
             self.king_check = {'Black': [], 'White': []}
             self.turn_count = 0
-
-    class Dummy():
-
-        """
-        This is the object used in blank spaces, given that it doesn't really take up any memory it's worth it for
-        the convenience alone
-        """
-
-        def __init__(self):
-            self.owner = 'None'
-            self.piece = "Not a Piece"
-            self.value = 0
-        def __str__(self):
-            return ' '
-
-        def getpos(self, current_state_raw):
-            self.position = "".join([k for k, v in current_state_raw.items() if self == current_state_raw[k]])
 
     class Piece():
         ''' Each subclass of the piece object has it\'s own function for checking the movment range against the
@@ -103,6 +85,20 @@ class Chessgame():
                 else:
                     up_right, up_left, down_right, down_left, up, down, left, right = -1,-1,-1,-1,-1,-1,-1,-1
 
+    class Dummy(Piece):
+
+        """
+        This is the object used in blank spaces, given that it doesn't really take up any memory it's worth it for
+        the convenience alone
+        """
+
+        def __init__(self):
+            self.owner = 'None'
+            self.piece = "Not a Piece"
+            self.value = 0
+        def __str__(self):
+            return ' '
+
     class Pawn(Piece):
         def __init__(self, playerID):
             super().__init__(playerID)
@@ -114,14 +110,13 @@ class Chessgame():
             if self.owner == 'Black':
                 foreward = [chr(ord(self.position[0]) - 1) + self.position[1]]
                 if self.position[0] == 'g': foreward.append(chr(ord(self.position[0]) - 2) + self.position[1])
-
                 foreward = [x for x in foreward if x[0] in 'abcdefgh' and x[1] in '12345678'
                             and (current_state_raw[x]).owner == 'None']
                 potatck = [chr(ord(self.position[0]) - 1) + str(int(self.position[1]) + 1),
                            chr(ord(self.position[0]) - 1) + str(int(self.position[1]) - 1)]
                 [foreward.append(x) for x in potatck if x[0] in 'abcdefgh' and x[1] in '12345678' and
                  (current_state_raw[x]).owner == 'White']
-            if self.owner == 'White':
+            elif self.owner == 'White':
                 foreward = [chr(ord(self.position[0]) + 1) + self.position[1]]
                 if self.position[0] == 'b': foreward.append(chr(ord(self.position[0]) + 2) + self.position[1])
                 foreward = [x for x in foreward if x[0] in 'abcdefgh' and x[1] in '12345678' and
@@ -180,6 +175,37 @@ class Chessgame():
             self.piece = 'Kng'
             self.value = 10000
 
+    def _setup(self):
+        '''
+        This builds the intial chess board, calling constructors for all the chess pieces as well as a 'Dummy' piece to
+        occupy empty spaces
+
+        '''
+        y = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        y.reverse() # sanity check this later
+        for i in range(0, len(y)):
+            self.board.append([y[i] + str(x) for x in range(1, 9)])
+        back_row = lambda owner: [self.Rook(owner), self.Knight(owner), self.Bishop(owner),
+                                  self.Queen(owner), self.King(owner), self.Bishop(owner), self.Knight(owner),
+                                  self.Rook(owner)]
+
+        for y in range(1, 9):
+            self.board_state.append(back_row('White')) if (y == 1) else \
+            self.board_state.append([self.Pawn('White') for i in range(1, 9)]) if (y == 2) else \
+            self.board_state.append([self.Pawn('Black') for i in range(1, 9)]) if (y == 7) else \
+            self.board_state.append(back_row('Black')) if (y == 8) else \
+            self.board_state.append([self.Dummy() for x in range(1, 9)])
+
+        tracker = {}
+        for (a, b) in zip([xx for x in self.board for xx in x], [yy for y in self.board_state for yy in y]):
+            tracker[a] = b
+            if a in list(zip(*self.board))[-1]:
+                self._current_state_raw.update(
+                    tracker)  # the list of single dictionaries used for  tracking where everything is
+                tracker = {}
+            [v.getpos(self._current_state_raw) for k, v in self._current_state_raw.items()]
+        self.get_current_state(self._current_state_raw)
+
     def _king_check_function(self):
 
         self.king_check[self.current_player] = []
@@ -194,38 +220,6 @@ class Chessgame():
         else:
             self.is_in_check = False
 
-    def _position_setup(self):
-        '''
-        This builds the intial chess board, calling constructors for all the chess pieces as well as a 'Dummy' piece to
-        occupy empty spaces
-
-        '''
-        back_row = lambda owner: [self.Rook(owner), self.Knight(owner), self.Bishop(owner),
-                                  self.Queen(owner), self.King(owner), self.Bishop(owner), self.Knight(owner),
-                                  self.Rook(owner)]
-
-        for y in range(1, 9):
-            self.board_state.append(back_row('White')) if (y == 1) else \
-                self.board_state.append([self.Pawn('White') for i in range(1, 9)]) if (y == 2) else \
-                    self.board_state.append([self.Pawn('Black') for i in range(1, 9)]) if (y == 7) else \
-                        self.board_state.append(back_row('Black')) if (y == 8) else \
-                            self.board_state.append([self.Dummy() for x in range(1, 9)])
-        tracker = {}
-        for (a, b) in zip([xx for x in self.board for xx in x], [yy for y in self.board_state for yy in y]):
-            tracker[a] = b
-            # TODO fix the updater so it can properly print out strings
-            if a in list(zip(*self.board))[-1]:
-                self._current_state_raw.update(
-                    tracker)  # the list of single dictionaries used for  tracking where everything is
-                tracker = {}
-            [v.getpos(self._current_state_raw) for k, v in self._current_state_raw.items()]
-        self.get_current_state(self._current_state_raw)
-
-    def _board_setup(self):
-        y = ["a", "b", "c", "d", "e", "f", "g", "h"]
-        for i in range(0, len(y)):
-            self.board.append([y[i] + str(x) for x in range(1, 9)])
-
     def get_current_state(self, raw_state):
 
         k, v = [k for k, v in raw_state.items()], [v for k, v in raw_state.items()]
@@ -237,101 +231,50 @@ class Chessgame():
                 self.current.append(row)
                 row = {}
 
-
-
     def piece_selector(self):
 
         if self.pseudoAI == 'yes':
-            choices = [k for k, v in self._current_state_raw.items() if v.owner == self.current_player]
+            choices = [pos for pos,piece in self._current_state_raw.items() if piece.owner == self.current_player]
             select = choices[r.randint(0, len(choices) - 1)]
             self.current_piece = self._current_state_raw[select]
-            return
-
-        while 1 == 1:
-
-            select = input('What piece would you like to move ?\n'
-                           '(select piece from coordinates or 0 to exit)')
-            if select == '0': exit()
-            if select not in self._current_state_raw.keys():
-                print('Invalid cordinates')
-                continue
-            elif (self._current_state_raw[select]).owner != self.current_player:
-                print('This is not you piece!')
-            else:
-                self.current_piece = self._current_state_raw[select]
-                return
 
     def move_selector(self):
         # TODO - Allow other pieces to be used to get the king out of check(if in check)
+
+
         potential_moves = self.current_piece.move_range(self._current_state_raw)
 
         if self.pseudoAI == 'yes':
             if self.is_in_check:
                 self.current_piece = self.king
-                potential_moves = (self.current_piece).move_range(self._current_state_raw)
-                potential_moves = [x for x in potential_moves if x not in self.king_check[self.current_player]]
-                if not potential_moves:
+            if self.current_piece.piece == 'Kng':
+                potential_moves = [move for move in self.current_piece.move_range(self._current_state_raw)
+                                   if move not in self.king_check[self.current_player]]
+                if not potential_moves and self.is_in_check: # Game over if king is in check and has no moves
                     print('Player ' + self.current_player + ' has been forced into checkmate')
                     leave = 1
                     return leave
-            elif (self.current_piece).piece == 'Kng':
-                potential_moves = (self.current_piece).move_range(self._current_state_raw)
-                potential_moves = [x for x in potential_moves if x not in self.king_check[self.current_player]]
-                while not potential_moves:
-                    self.piece_selector()
-                    potential_moves = (self.current_piece).move_range(self._current_state_raw)
-            else:
-                while not potential_moves:
-                    self.piece_selector()
-                    potential_moves = (self.current_piece).move_range(self._current_state_raw)
-            # print(s.__str__(True, potential_moves))
+            while not potential_moves: # TODO see if this the selector is leading to kings dying
+                self.piece_selector()
+                potential_moves = self.current_piece.move_range(self._current_state_raw) # Is this breaking the checkmate?
 
-            for x in potential_moves:
-                if (self._current_state_raw[x]).owner != self.current_player and self._current_state_raw[x] != 'None':
-                    move = x
+            for move in potential_moves:
+                if (self._current_state_raw[move]).owner != self.current_player \
+                        and (self._current_state_raw[move]).owner != 'None':
+                    break
                 else:
                     move = potential_moves[r.randint(0, len(potential_moves) - 1)]
-            if (self.current_piece).piece == 'Pwn':
-                if move[0] in 'ah':
-                    holder = self.current_piece.position
-                    self.current_piece = self.Queen(self.current_player)
-                    (self.current_piece).position = holder
-            replacer = self.current_piece.position
-            self._current_state_raw[move] = self.current_piece
-            (self.current_piece).position = move
-            self._current_state_raw[replacer] = self.Dummy()
-            for dict in self.current:
-                for x in dict.keys(): dict[x] = self._current_state_raw[x]
-            if self.pseudoAI == 'no': print(self)
-            return
 
-        #TODO match this section with the one above
-        while 1 == 1:
-            if self.is_in_check:
-                self.current_piece = self.king
-                potential_moves = (self.current_piece).move_range(self._current_state_raw)
-                if not potential_moves:
-                    exit('Player' + self.current_player + 'Has been forced into checkmate')
-            move = input('Select where you would like to move this piece \n'
-                         'or imput \'moves\' to show possible moves ')
-            if move == '0':
-                exit(print('Thanks for playing!'))
-            if move == 'moves':
-                print(s.__str__(True, potential_moves))
-            elif move not in potential_moves:
-                print('This is not a valid destination')
+            if (self.current_piece).piece == 'Pwn' and move[0] in 'ah':
+                temp = self.current_piece.position
+                self.current_piece = self.Queen(self.current_player)
+                self.current_piece.position = move
             else:
-                confirm = input('Confirm move' + (self.current_piece).position + '--->' + move)
-                if 'y' in confirm:
-                    self._current_state_raw[move] = self.current_piece
-                    self._current_state_raw[(self.current_piece).position] = self.Dummy()
-                    for dict in self.current:
-                        for x in dict.keys(): dict[x] = self._current_state_raw[x]
-
-                    print(self)
-                    return
-                    # this will likely be how changes are incorporated
-
+                temp = self.current_piece.position
+                self._current_state_raw[move] = self.current_piece
+                self.current_piece.position = move
+            self._current_state_raw[temp] = self.Dummy()
+            self.get_current_state(self._current_state_raw)
 
     def __str__(self, showmoves=False, moves=[]):
 
